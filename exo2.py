a31b9feb10d44f15ba1d9d180f6eda36bc0dac51
argonaultes <gael@argonaultes.fr>
2024-01-29 14:54:06 +0100

    more concise to create spaceships dico

--- exo2.py
+++ exo2.py
@@ -3,12 +3,12 @@ spaceships = []
 spaceshipWithMaxRatio = spaceships[0]
 
 for idx in range(3):
-    spaceship = {} # déclaration du dictionnaire vaisseau
-
-    spaceship['color'] = input('color: ')
-    spaceship['nb_passengers'] = int(input('passengers: '))
-    spaceship['nb_seats'] = int(input('seats: '))
-    spaceship['name'] = input('name')
+    spaceship = {
+        'color': input('color: '),
+        'nb_passengers': int(input('passengers: ')),
+        'nb_seats':  int(input('seats: ')),
+        'name': input('name')
+    }
     spaceship['ratio_passenger_seat'] = spaceship['nb_passengers'] / spaceship['nb_seats']
 
     # est-ce que le nouveau vaisseau est plus rempli que les autres déjà créés
