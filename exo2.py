4689e512dc1ce522d0a1b1721fc321d8d3bbcbaf
argonaultes <gael@argonaultes.fr>
2024-01-29 15:01:51 +0100

    use condensed syntax for list

--- exo2.py
+++ exo2.py
@@ -1,14 +1,13 @@
-spaceships = []
-
 spaceshipWithMaxRatio = spaceships[0]
 
-for idx in range(3):
-    spaceship = {
+spaceships = [{
         'color': input('color: '),
         'nb_passengers': int(input('passengers: ')),
         'nb_seats':  int(input('seats: ')),
-        'name': input('name')
-    }
+        'name': input('name'),
+    } for _ in range(3)]
+
+for idx in range(3):
     spaceship['ratio_passenger_seat'] = spaceship['nb_passengers'] / spaceship['nb_seats']
 
     # est-ce que le nouveau vaisseau est plus rempli que les autres déjà créés
