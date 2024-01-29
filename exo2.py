6107e4491b2dafe4ae1f6cd5128c092978938bed
argonaultes <gael@argonaultes.fr>
2024-01-29 14:52:29 +0100

    other method to create hash item

--- exo2.py
+++ exo2.py
@@ -1,15 +1,22 @@
-spaceships = [{}, {}, {}]
+spaceships = []
 
 spaceshipWithMaxRatio = spaceships[0]
 
-for spaceship in spaceships:
+for idx in range(3):
+    spaceship = {} # déclaration du dictionnaire vaisseau
+
     spaceship['color'] = input('color: ')
     spaceship['nb_passengers'] = int(input('passengers: '))
     spaceship['nb_seats'] = int(input('seats: '))
     spaceship['name'] = input('name')
     spaceship['ratio_passenger_seat'] = spaceship['nb_passengers'] / spaceship['nb_seats']
 
+    # est-ce que le nouveau vaisseau est plus rempli que les autres déjà créés
     if spaceshipWithMaxRatio['ratio_passenger_seat'] < spaceship['ratio_passenger_seat']:
         spaceshipWithMaxRatio = spaceship
+
+    # ajout en fin de liste
+    spaceships.append(spaceship)
+    
 
 print(f'max ratio: {spaceshipWithMaxRatio["name"]}')
