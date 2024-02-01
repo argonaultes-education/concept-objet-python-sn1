b82308a3b986f76cec764505bf91e499b02e0430
argonaultes <gael@argonaultes.fr>
2024-02-01 15:08:02 +0100

    using class now

--- exo2.py
+++ exo2.py
@@ -1,27 +1,36 @@
+# créer la classe Spaceship
 
-spaceships = [{
-        'color': input('color: '),
-        'nb_passengers': int(input('passengers: ')),
-        'nb_seats':  int(input('seats: ')),
-        'name': input('name'),
-    } for _ in range(3)]
-
-spaceshipWithMaxRatio = spaceships[0]
-
-
-for idx in range(3):
-    # exploded
-    spaceship = spaceships[idx]
-    spaceship['ratio_passenger_seat'] = spaceship['nb_passengers'] / spaceship['nb_seats']
-    # condensed
-    spaceships[idx]['ratio_passenger_seat'] = spaceships[idx]['nb_passengers'] / spaceships[idx]['nb_seats']
-
-    # est-ce que le nouveau vaisseau est plus rempli que les autres déjà créés
-    if spaceshipWithMaxRatio['ratio_passenger_seat'] < spaceship['ratio_passenger_seat']:
-        spaceshipWithMaxRatio = spaceship
-
-    # ajout en fin de liste
-    spaceships.append(spaceship)
+class Spaceship:
     
+    def __init__(self, color, nb_passengers, nb_seats, name):
+        self.color = color
+        self.nb_passengers = nb_passengers
+        self.nb_seats = nb_seats
+        self.name = name
+    
+# utiliser la classe à la place du dictionnaire pour chaque élément de la liste
 
-print(f'max ratio: {spaceshipWithMaxRatio["name"]}')
+spaceships = [ Spaceship(
+                    input('color: '),
+                    int(input('passengers: ')),
+                    int(input('seats: ')),
+                    input('name: '),
+            ) for _ in range(3) ]
+
+
+# spaceships = []
+# for _ in range(3):
+#     spaceship = Spaceship(
+#                     input('color: '),
+#                     int(input('passengers: ')),
+#                     int(input('seats: ')),
+#                     input('name: '),
+#             )
+#     spaceships.append(spaceship)
+
+
+# modifier le code pour interroger les attributs correspondants
+
+spaceshipWithMaxRatio = max(spaceships, key=lambda spaceship: spaceship.nb_passengers / spaceship.nb_seats)
+
+print(f'max ratio: {spaceshipWithMaxRatio.name}')
