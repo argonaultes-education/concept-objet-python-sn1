f3fea1286d782fbec8d5bbe4adec5a04859e9895
argonaultes <gael@argonaultes.fr>
2024-02-09 12:27:39 +0100

    waiting for set hash __eq__

--- superhyperplanning/classe.py
+++ superhyperplanning/classe.py
@@ -1,2 +1,11 @@
 class Classe:
-    pass
+    
+    def __init__(self, ecole, nom):
+        self.__ecole = ecole
+        self.__nom = nom
+
+    # def __eq__(self, other):
+    #     pass
+
+    def __str__(self):
+        return f'Classe: {self.__nom}'
--- superhyperplanning/ecole.py
+++ superhyperplanning/ecole.py
@@ -1,16 +1,33 @@
+from groupe import Groupe
+from classe import Classe
+
 class Ecole:
     
     def __init__(self, groupe, nom):
         self.__groupe = groupe
         self.__groupe.ajout_ecole(self)
         self.__nom = nom
+        self.__classes = set()
+
+    def ajout_classe(self, classe):
+        self.__classes.add(classe)
 
     @property
     def nom(self):
         return self.__nom
     
+    def afficher_classes(self):
+        for classe in self.__classes:
+            print(classe)
+
     def display(self):
         print(f'Nom: {self.__nom}')
 
     def __str__(self):
         return f'Nom: {self.__nom}'
+
+cetd = Groupe('cetd')
+epsi = Ecole(cetd, 'epsi')
+epsi.ajout_classe(Classe(epsi, 'sn1-gr1'))
+epsi.ajout_classe(Classe(epsi, 'sn1-gr1'))
+epsi.afficher_classes()
